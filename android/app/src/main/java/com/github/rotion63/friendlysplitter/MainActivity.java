ec9b3551cd8a84beb9c0f7b3c68fb5647be2b6ef
Madhav Prasad Guragain <guragainroshan63@gmail.com>
2025-03-26 09:21:19 +0545

    Before upgrading the gradle to 8.9.0 from 8.7.2

---
+++ android/app/src/main/java/com/github/rotion63/friendlysplitter/MainActivity.java
@@ -0,0 +1,5 @@
+package com.github.rotion63.friendlysplitter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.github.rotion63.friendlysplitter',
+  appName: 'कति भो बिल ?',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+ };
+
+export default config;
